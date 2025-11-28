5c2f9841db7fd770fbfd93119cabcea267f04f80
Boluwayinka7080 <boluwatifeodunlami82@gmail.com>
2025-11-28 17:31:45 +0100

    assignments

---
+++ customerissue.js
@@ -0,0 +1,24 @@
+let FirstNumber = 5;
+let SecondNumber = 7;
+let TheSumDouble = (FirstNumber + SecondNumber) * 2;
+console.log(TheSumDouble);
+
+let number = 20
+let calculation = ( number  % 10) + 20;
+console.log(calculation);
+
+
+let integer = 5;
+let exponent = 3;
+let multiplication = (integer * integer) ** 3;
+console.log(multiplication);
+
+
+let IntegerA = 30;
+let IntegerX = IntegerA + 10;
+let IntegerY = IntegerA - 10;
+let IntegerZ = IntegerA % 10;
+
+let mathematical =  IntegerA / (IntegerX * IntegerY * IntegerZ);
+console.log(mathematical);
+
